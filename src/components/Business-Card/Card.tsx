3cc7607c16000a6f0719125f59dfdaade5c736a6
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-06-28 13:01:01 +0200

    conditional rendering practice

--- src/components/Business-Card/Card.tsx
+++ src/components/Business-Card/Card.tsx
@@ -1,5 +1,16 @@
 import Stack from "./Stack";
 
+const stacks = [
+    {name: 'HTML', color: '#4e5ccc', level: 'advanced'},
+    {name: 'JS', color: '#fcba03', level: 'intermediate'},
+    {name: 'CSS', color: '#bd3e49', level: 'advanced'},
+    {name: 'SCSS', color: '#933ebd', level: 'intermediate'},
+    {name: 'React', color: '#61dbfb', level: 'beginner'},
+    {name: 'Vue', color: '#026e00', level: 'beginner'},
+    {name: 'Angular', color: '#dd0031', level: 'intermediate'},
+    {name: 'Firebase', color: '#777', level: 'beginner'},
+];
+
 export default function Card() {
     return (
         <div>
@@ -11,11 +22,24 @@ export default function Card() {
                     <h5 className={"card-title"}> Kriszti </h5>
                     <p className={"card-text"}> Huhu, Mein Name ist Kriszti, Full Stack Entwicklerin. Ich lerne gerade
                         React, und finde es mega toll </p>
-                    <section className={"d-flex justify-content-evenly"}>
-                        <Stack name={"HTML"} color={'lightblue'}/>
-                        <Stack name={"JS"} color={'purple'}/>
-                        <Stack name={"CSS"} color={'yellow'}/>
-                        <Stack name={"React"} color={'red'}/>
+                    {/*<section className={"d-flex justify-content-evenly"}>*/}
+                    {/*    <Stack name={"HTML"} color={'lightblue'}/>*/}
+                    {/*    <Stack name={"JS"} color={'purple'}/>*/}
+                    {/*    <Stack name={"CSS"} color={'yellow'}/>*/}
+                    {/*    <Stack name={"React"} color={'red'}/>*/}
+                    {/*</section>*/}
+                    <section className={"container"}>
+                        <div className={"row"}>
+                            {stacks.map((stack, index) => (
+                               <div key={index} className={"col-4 mb-1"}>
+                                <Stack
+                                    name={stack.name}
+                                    color={stack.color}
+                                    level={stack.level}
+                                />
+                               </div>
+                            ))}
+                        </div>
                     </section>
                 </div>
             </div>
--- src/components/Business-Card/Stack.tsx
+++ src/components/Business-Card/Stack.tsx
@@ -1,7 +1,12 @@
-export default function Stack({name, color}: {name: string; color: string}) {
+export default function Stack({name, color, level}: {name: string; color: string; level: string}) {
     return (
         <div>
-            <button className={"btn btn-light"} style={{backgroundColor: color }}> {name} </button>
+            <button className={"btn btn-light"} style={{backgroundColor: color }}>
+                {name}
+                {level === 'advanced' && <span>🏆</span>}
+                {level === 'intermediate' && <span>🏅</span>}
+                {level === 'beginner' && <span> 📙</span>}
+            </button>
         </div>
     );
 }

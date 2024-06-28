377185f22e33f2bf52b4813cc3e55ac543601bbf
SzathmaryKriszti <krisztacsuri@gmail.com>
2024-06-28 09:48:58 +0200

    conditional rendering with ternary operator

--- src/components/Fruits-project/FruitCard.tsx
+++ src/components/Fruits-project/FruitCard.tsx
@@ -2,17 +2,29 @@ export default function FruitCard({name, amount, image}: { name: string; amount:
 
     return (
         <div>
-            <div className={"card text-center"} style={{width: '18rem'}}>
+            <div className={"card text-center"} style={{width: '18rem', height: '28rem'}}>
                 <img src={image} alt={name}/>
                 <div className={"card-body"}>
                     <h5 className={"card-title"}> {name} </h5>
                     <p className={"card-text"}>Amount: {amount}</p>
-                    {amount > 0 &&
+                    {/*{amount > 0 && (*/}
+                    {/*    <>*/}
+                    {/*        <h6 className={"card-subtitle mb-1"}> Discount!</h6>*/}
+                    {/*        <button className={"btn btn-success"}> Shop</button>*/}
+                    {/*    </>*/}
+                    {/*)}*/}
+                    {amount > 0 ? (
                         <>
                             <h6 className={"card-subtitle mb-1"}> Discount!</h6>
                             <button className={"btn btn-success"}> Shop</button>
                         </>
-                    }
+                    ) : (
+                        <>
+                            <h6 className={"card-subtitle mb-1"}> Unavailable!</h6>
+                            <button disabled className={"btn btn-danger"}> Out of stock</button>
+                        </>
+                    )}
+
                 </div>
             </div>
         </div>
